6997441129c1b968caa8f84c1420eea8f6aeef8b
sadko4u <sadko4u@gmail.com>
2021-05-22 16:02:09 +0300

    Added basic processing functions

---
+++ include/private/audio.h
@@ -0,0 +1,99 @@
+/*
+ * Copyright (C) 2021 Linux Studio Plugins Project <https://lsp-plug.in/>
+ *           (C) 2021 Vladimir Sadovnikov <sadko4u@gmail.com>
+ *
+ * This file is part of far-screamer
+ * Created on: 22 мая 2021 г.
+ *
+ * far-screamer is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * any later version.
+ *
+ * far-screamer is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with far-screamer. If not, see <https://www.gnu.org/licenses/>.
+ */
+
+#ifndef PRIVATE_AUDIO_H_
+#define PRIVATE_AUDIO_H_
+
+#include <lsp-plug.in/common/status.h>
+#include <lsp-plug.in/runtime/LSPString.h>
+#include <lsp-plug.in/dsp-units/sampling/Sample.h>
+#include <lsp-plug.in/dsp-units/filters/Equalizer.h>
+#include <lsp-plug.in/expr/Resolver.h>
+
+namespace far_screamer
+{
+    using namespace lsp;
+
+    /**
+     * Load audio file
+     *
+     * @param sample sample to store audio data
+     * @param srate desired sample rate
+     * @param base base directory
+     * @param name name of the file
+     * @return status of operation
+     */
+    status_t load_audio_file(dspu::Sample *sample, size_t srate, const LSPString *base, const LSPString *name);
+
+    /**
+     * Save audio file
+     *
+     * @param sample sample to save
+     * @param base base directory
+     * @param fmt output file name format
+     * @param vars variable to parametrize the output file name format
+     * @return status of operation
+     */
+    status_t save_audio_file(dspu::Sample *sample, const LSPString *base, const LSPString *fmt, expr::Resolver *vars);
+
+    /**
+     * Convolve the specified channel of input audio file with specified channel of the impulse response
+     * and add result to specified channel of the output file
+     *
+     * @param dst destination sample to add convolution data
+     * @param src source sample to use for convolution
+     * @param ir impulse response to use for convolution
+     * @param dst_ch the number of destination channel
+     * @param src_ch the number of source channel
+     * @param ir the number of channel in the impulse response
+     * @param predelay the predelay in samples of the convolved data
+     * @param gain the overall gain of convolution (1.0f = 0 dB)
+     * @return status of operation
+     */
+    status_t convolve(
+        dspu::Sample *dst, const dspu::Sample *src, const dspu::Sample *ir,
+        size_t dst_ch, size_t src_ch, size_t ir_ch,
+        size_t predelay, float gain
+    );
+
+
+    /**
+     * Add latency to the sample
+     * @param dst destination sample to apply latency
+     * @param latency latency (in samples) to add
+     * @param gain the gain to adjust (1.0f = 0 dB)
+     * @return status of operation
+     */
+    status_t adjust_latency_gain(dspu::Sample *dst, size_t latency, float gain);
+
+    /**
+     * Apply filters to the audio sample
+     * @param dst destination sample to apply filters
+     * @param eq filters to apply
+     * @return status of operation
+     */
+    status_t filter_sample(dspu::Sample *dst, dspu::Equalizer *eq);
+
+}
+
+
+
+#endif /* PRIVATE_AUDIO_H_ */
---
+++ src/main/audio.cpp
@@ -0,0 +1,315 @@
+/*
+ * Copyright (C) 2021 Linux Studio Plugins Project <https://lsp-plug.in/>
+ *           (C) 2021 Vladimir Sadovnikov <sadko4u@gmail.com>
+ *
+ * This file is part of far-screamer
+ * Created on: 22 мая 2021 г.
+ *
+ * far-screamer is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * any later version.
+ *
+ * far-screamer is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with far-screamer. If not, see <https://www.gnu.org/licenses/>.
+ */
+
+#include <private/audio.h>
+#include <lsp-plug.in/stdlib/stdio.h>
+#include <lsp-plug.in/common/alloc.h>
+#include <lsp-plug.in/dsp/dsp.h>
+#include <lsp-plug.in/expr/Expression.h>
+#include <lsp-plug.in/dsp-units/misc/windows.h>
+#include <lsp-plug.in/dsp-units/misc/fade.h>
+#include <lsp-plug.in/dsp-units/util/Convolver.h>
+
+namespace far_screamer
+{
+    using namespace lsp;
+
+    typedef struct duration_t
+    {
+        size_t h;
+        size_t m;
+        size_t s;
+        size_t ms;
+    } duration_t;
+
+    void calc_duration(duration_t *d, const dspu::Sample *sample)
+    {
+        uint64_t duration = (uint64_t(sample->samples()) * 1000) / sample->sample_rate();
+        d->ms = duration % 1000;
+        duration /= 1000;
+        d->s = duration % 60;
+        duration /= 60;
+        d->m = duration % 60;
+        d->h = duration / 60;
+    }
+
+    status_t load_audio_file(dspu::Sample *sample, size_t srate, const LSPString *base, const LSPString *name)
+    {
+        status_t res;
+        io::Path path;
+
+        // Generate file name
+        if ((res = path.set(name)) != STATUS_OK)
+        {
+            fprintf(stderr, "  could not read file '%s', error code: %d\n", name->get_native(), int(res));
+            return res;
+        }
+        if (!path.is_absolute())
+        {
+            if ((res = path.set(base, name)) != STATUS_OK)
+            {
+                fprintf(stderr, "  could not read file '%s', error code: %d\n", name->get_native(), int(res));
+                return res;
+            }
+        }
+
+        // Load sample from file
+        if ((res = sample->load(&path)) != STATUS_OK)
+        {
+            fprintf(stderr, "  could not read file '%s', error code: %d\n", path.as_native(), int(res));
+            return res;
+        }
+
+        duration_t d;
+        calc_duration(&d, sample);
+        fprintf(stdout, "  loaded file: '%s', channels: %d, samples: %d, sample rate: %d, duration: %02d:%02d:%02d.%03d\n",
+                path.as_native(),
+                int(sample->channels()), int(sample->length()), int(sample->sample_rate()),
+                int(d.h), int(d.m), int(d.s), int(d.ms)
+        );
+
+        // Resample audio data
+        if ((res = sample->resample(srate)) != STATUS_OK)
+        {
+            fprintf(stderr, "  could not resample file '%s' to sample rate %d, error code: %d\n",
+                    path.as_native(), int(srate), int(res)
+            );
+            return res;
+        }
+
+        return STATUS_OK;
+    }
+
+    status_t save_audio_file(dspu::Sample *sample, const LSPString *base, const LSPString *fmt, expr::Resolver *vars)
+    {
+        status_t res;
+        expr::Expression x;
+        expr::value_t val;
+        LSPString fname;
+        io::Path path, dir;
+
+        // Parse the expression
+        if ((res = x.parse(fmt, expr::Expression::FLAG_STRING)) != STATUS_OK)
+        {
+            fprintf(stderr, "  invalid expression: '%s'\n", fmt->get_native());
+            return STATUS_BAD_FORMAT;
+        }
+
+        // Evaluate the expression and cast to string
+        expr::init_value(&val);
+        x.set_resolver(vars);
+        if ((res = x.evaluate(&val)) == STATUS_OK)
+            res = expr::cast_string(&val);
+        if (res != STATUS_OK)
+        {
+            expr::destroy_value(&val);
+            fprintf(stderr, "  could not evaluate expression: '%s'\n", fmt->get_native());
+            return STATUS_BAD_FORMAT;
+        }
+        fname.swap(val.v_str);
+        expr::destroy_value(&val);
+
+        // Generate file name
+        if ((res = path.set(&fname)) != STATUS_OK)
+        {
+            fprintf(stderr, "  could not write file '%s', error code: %d\n", fname.get_native(), int(res));
+            return res;
+        }
+        if (!path.is_absolute())
+        {
+            if ((res = path.set(base, &fname)) != STATUS_OK)
+            {
+                fprintf(stderr, "  could not write file '%s', error code: %d\n", fname.get_native(), int(res));
+                return res;
+            }
+        }
+
+        // Create parent directory recursively
+        res = path.get_parent(&dir);
+        if (res == STATUS_OK)
+        {
+            if ((res = dir.mkdir(true)) != STATUS_OK)
+            {
+                fprintf(stderr, "  could not create directory '%s', error code: %d\n", dir.as_native(), int(res));
+                return res;
+            }
+        }
+        else if (res != STATUS_NOT_FOUND)
+        {
+            fprintf(stderr, "  could not obtain parent directory for file '%s', error code: %d\n", fname.get_native(), int(res));
+            return res;
+        }
+
+        // Load sample from file
+        if ((res = sample->save(&path)) < 0)
+        {
+            fprintf(stderr, "  could not write file '%s', error code: %d\n", path.as_native(), int(-res));
+            return -res;
+        }
+
+        duration_t d;
+        calc_duration(&d, sample);
+        fprintf(stdout, "  saved file: '%s', channels: %d, samples: %d, sample rate: %d, duration: %02d:%02d:%02d.%03d\n",
+                path.as_native(),
+                int(sample->channels()), int(sample->length()), int(sample->sample_rate()),
+                int(d.h), int(d.m), int(d.s), int(d.ms)
+        );
+
+        return STATUS_OK;
+    }
+
+    status_t convolve(
+        dspu::Sample *dst, const dspu::Sample *src, const dspu::Sample *ir,
+        size_t dst_ch, size_t src_ch, size_t ir_ch,
+        size_t predelay, float gain
+    )
+    {
+        dspu::Sample out;
+        dspu::Convolver cv;
+
+        // Check channel numbers
+        if (src_ch >= src->channels())
+        {
+            fprintf(stderr, "Invalid channel number for input file: %d", int(src_ch));
+            return STATUS_BAD_ARGUMENTS;
+        }
+        if (ir_ch >= ir->channels())
+        {
+            fprintf(stderr, "Invalid channel number for impulse response file: %d", int(ir_ch));
+            return STATUS_BAD_ARGUMENTS;
+        }
+
+        // Allocate necessary buffers
+        size_t dry_length   = src->length();
+        size_t wet_length   = dry_length + ir->length(); // The length of wet (processed) signal
+        size_t length       = lsp_max(dry_length, wet_length);
+
+        // Allocate the necessary space for the output sample
+        size_t num_ch       = lsp_max(dst_ch + 1, dst->channels());
+        if ((num_ch != dst->channels()) || (length > dst->length()))
+        {
+            status_t res = dst->resize(num_ch, length, length);
+            if (res != STATUS_OK)
+            {
+                fprintf(stderr, "Not enough memory to resize the output audio data");
+                return res;
+            }
+        }
+
+        // Allocate buffer for convolution tail
+        uint8_t *ptr;
+        float *buf          = alloc_aligned<float>(ptr, length);
+        if (buf == NULL)
+        {
+            fprintf(stderr, "Not enough memory to allocate temporary buffer");
+            return STATUS_NO_MEM;
+        }
+
+        // Perform convolution
+        if (!cv.init(ir->channel(ir_ch), ir->length(), 16, 0))
+        {
+            free_aligned(ptr);
+            fprintf(stderr, "Not enough memory to initialize convolver");
+            return STATUS_NO_MEM;
+        }
+        dsp::fill_zero(buf, length); // Fill head of buffer with zeros
+        cv.process(&buf[0], src->channel(src_ch), dry_length); // The main convolution
+        cv.process(&buf[dry_length], &buf[dry_length], ir->length()); // The tail of convolution
+
+        // Apply convolution to the output sample
+        float *dptr = dst->channel(dst_ch);
+        dsp::fmadd_k3(&dptr[predelay], buf, gain, length);
+
+        // Free temporary buffer
+        free_aligned(ptr);
+
+        return STATUS_OK;
+    }
+
+    status_t adjust_latency_gain(dspu::Sample *dst, size_t latency, float gain)
+    {
+        size_t channels     = dst->channels();
+        size_t length       = dst->length();
+        size_t new_length   = latency + length;
+
+        if (channels <= 0)
+            return STATUS_OK;
+
+        // Resize sample to the new size with added amount of latency
+        if (length != new_length)
+        {
+            status_t res = dst->resize(channels, new_length, new_length);
+            if (res != STATUS_OK)
+            {
+                fprintf(stderr, "Could not resize audio sample to %d channels, %d samples",
+                        int(channels), int(new_length));
+                return res;
+            }
+        }
+
+        // Apply latency and gain to the sample
+        for (size_t i=0; i<channels; ++i)
+        {
+            float *buf  = dst->channel(i);
+            if (gain != 1.0f) // Apply gain
+                dsp::mul_k2(&buf[0], gain, length);
+            dsp::move(&buf[latency], &buf[0], length);  // Apply latency
+            dsp::fill_zero(&buf[0], latency);           // Pad with zeros
+        }
+
+        return STATUS_OK;
+    }
+
+    status_t filter_sample(dspu::Sample *dst, dspu::Equalizer *eq)
+    {
+        size_t channels     = dst->channels();
+        size_t length       = dst->length();
+
+        if ((channels <= 0) || (eq->mode() == dspu::EQM_BYPASS))
+            return STATUS_OK;
+
+        // Resize sample to the new size with added amount of latency and IR length
+        size_t new_length   = length + eq->get_latency() + eq->ir_size();
+        if (length != new_length)
+        {
+            status_t res = dst->resize(channels, new_length, new_length);
+            if (res != STATUS_OK)
+            {
+                fprintf(stderr, "Could not resize audio sample to %d channels, %d samples",
+                        int(channels), int(new_length));
+                return res;
+            }
+        }
+
+        // Perform audio filtering
+        for (size_t i=0; i<channels; ++i)
+        {
+            float *buf  = dst->channel(i);
+            eq->reset();                        // Reset the state of equalizer
+            eq->process(buf, buf, new_length);  // Process the channel by the equalizer
+        }
+
+        return STATUS_OK;
+    }
+}
+
+
+
